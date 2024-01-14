e365ce3f9d910295c6476bb30dfb91dcc7a7fbd4
tsfhd2024 <tsf.hd.2024@gmail.com>
2024-01-14 01:50:58 +0100

    ADD add bash script to launch final code

---
+++ scripts/run.sh
@@ -0,0 +1,10 @@
+m="ARHD"
+CUDA_VISIBLE_DEVICES=0 python3 -u main.py \
+--method $m \
+--root_path ./data/ \
+--test_bsz 1 \
+--data Exchange \
+--seq_len 6 \
+--pred_len 3 --learning_rate 1e-3 \
+--l2_lambda 2e-3 \
+--label_len 6
