1dc749b6e3cfaccaa11e08f1f043788b068d4d1f
tsfhd2024 <tsf.hd.2024@gmail.com>
2024-01-14 01:48:50 +0100

    ADD add utils

---
+++ utils/metrics.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def cumavg(m):
+    cumsum = np.cumsum(m)
+    return cumsum / np.arange(1, cumsum.size + 1)
+
+
+def RSE(pred, true):
+    return np.sqrt(np.sum((true - pred) ** 2)) / np.sqrt(
+        np.sum((true - true.mean()) ** 2)
+    )
+
+
+def CORR(pred, true):
+    mean_x = np.mean(pred)
+    mean_y = np.mean(true)
+    numerator = np.sum((pred - mean_x) * (true - mean_y))
+    denominator = np.sqrt(np.sum((pred - mean_x) ** 2) * np.sum((true - mean_y) ** 2))
+    r = numerator / denominator
+    return r
+
+
+def metric(pred, true):
+    rse = RSE(pred, true)
+    corr = CORR(pred, true)
+    return rse, corr
