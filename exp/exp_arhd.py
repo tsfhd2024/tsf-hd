bb65d9562a1b0fe718fc91242052ec2a5e70adb9
tsfhd2024 <tsf.hd.2024@gmail.com>
2024-01-14 01:45:00 +0100

    ADD add ARHD experiment

--- exp/exp_arhd.py
+++ exp/exp_arhd.py
@@ -8,8 +8,7 @@ import torch.nn as nn
 from torch import optim
 from tqdm import tqdm
 
-from data.data_loader import (Dataset_Custom, Dataset_ETT_hour,
-                              Dataset_ETT_minute)
+from data.data_loader import Dataset_Custom, Dataset_ETT_hour, Dataset_ETT_minute
 from exp.exp_basic import Exp_Basic
 from models import MultivariateARModel
 from utils.metrics import cumavg, metric
